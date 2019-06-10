2abac6dcc59b7cac0bfca0aa8a35870e14d747c3
Aliaksandr <aliaksandr.badretdzinau@gmail.com>
2019-06-10 12:41:49 +0300

    Added App component state which stores tasks items. Also added functions which sends messages into terminal when passed input data and added item

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import './App.css';
 import TodoList from './TodoList'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      items: [],
+      currentItem: {text:'', key:''},
+    }
+  }
+
+  handleInput = e => {
+    console.log('Hello Input')
+  }
+
+  addItem = () => {
+    console.log('Hello Add Item')
+  }
+
   render() {
     return (
       <div className="App">
